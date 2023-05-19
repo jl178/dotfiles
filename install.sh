5e9e09bafce64ea26678d14e983979c48d5ad2f3
jered.little <jeredlittle1996@gmail.com>
2023-05-19 15:27:07 -0600

    Add .p10k

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 ln -s ~/dotfiles/.config/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.config/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/.config/powerlevel10k ~/powerlevel10k
+ln -s ~/dotfiles/.config/zsh/.p10k.zsh ~/
+
