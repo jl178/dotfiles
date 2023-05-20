7adefeafc109e9d5878d4362bb9d0257c973f6a9
jered.little <jeredlittle1996@gmail.com>
2023-05-19 18:01:21 -0600

    change install for nvim

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ ln -s ~/dotfiles/.config/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.config/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/.config/powerlevel10k ~/powerlevel10k
 ln -s ~/dotfiles/.config/zsh/.p10k.zsh ~/
-mkdir ~/.config/nvim/lua/user/
-ln -s ~/dotfiles/.config/astrovim/lua/user/* ~/.config/nvim/lua/user/
-
+mkdir ~/.config/nvim/lua/
+ln -s ~/dotfiles/.config/astrovim/lua/user ~/.config/nvim/lua/
+mkdir -p ~/.tmux/plugins/
+ln -s ~/dotfiles/.config/tmux/.tmux/plugins/tpm ~/.tmux/plugins/
+ln -s ~/dotfiles/.config/tmux/ ~/.config/
